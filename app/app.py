4381132092c647708421baf72d660b9a98bfa104
kirill st. <staroshchukkirill@gmail.com>
2026-02-09 22:08:56 +0100

    upd workflow\app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevSecOps project is running!"
+    return "DevSecOps Pipeline is LIVE! v2.0"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
