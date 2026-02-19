f35ced8887fde7157925e88d385d024cda5bfef4
kirill st. <staroshchukkirill@gmail.com>
2026-02-19 22:11:13 +0100

    fix

--- app/app.py
+++ app/app.py
@@ -1,10 +1,47 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "OLEKSANDR SOSI HUI )))))"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8080)
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevSecOps Pipeline is LIVE! v2.0"
+    return """
+    <html>
+    <head>
+        <style>
+            body {
+                margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background-color: #f0f0f0;
+                font-family: sans-serif;
+            }
+            h1 {
+                font-size: 17vw;
+                text-align: center;
+                color: #000000;
+                text-transform: uppercase;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>АLEKSANDR SOSI HUI )))))</h1>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
